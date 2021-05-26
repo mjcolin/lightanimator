a128df41703b0662a53975342cc05dd52b949b3d
mjcolin <81725610+mjcolin@users.noreply.github.com>
2021-05-25 23:02:37 -0400

    made it so can have a frame with and without click

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,18 +1,25 @@
 import classes from './Frame.module.css'
 
 function Frame(props){
+    let thePixels = null;
 
-
-    let thePixels = props.pixels.map((pixel, index)=>
-        
-
+    if(props.onClick != null){
+         thePixels = props.pixels.map((pixel, index)=>
         <div className={classes.pixel} 
         style={{backgroundColor: pixel.hue, opacity: pixel.opacity+'%'}} 
         key={pixel.hue+index}
         onClick={()=>props.onClick(props.frame,index, (pixel.hue === 'yellow') ? 'black' : 'yellow', 100)}
-        
         ></div>
-    );
+         );
+    }
+    else{
+        //no click handler
+        thePixels = props.pixels.map((pixel, index)=>
+        <div className={classes.pixel} 
+        style={{backgroundColor: pixel.hue, opacity: pixel.opacity+'%'}} 
+        key={pixel.hue+index}></div>
+         );
+    }
 
 
     return(
