b2c3afc767b37341c5083dd4e44556234eb822c1
mjcolin <81725610+mjcolin@users.noreply.github.com>
2021-05-25 21:02:57 -0400

    made the lights toggle

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -2,11 +2,14 @@ import classes from './Frame.module.css'
 
 function Frame(props){
 
+
     let thePixels = props.pixels.map((pixel, index)=>
+        
+
         <div className={classes.pixel} 
         style={{backgroundColor: pixel.hue, opacity: pixel.opacity+'%'}} 
         key={pixel.hue+index}
-        onClick={()=>props.onClick(0,index, 'yellow', 100)}
+        onClick={()=>props.onClick(0,index, (pixel.hue === 'yellow') ? 'black' : 'yellow', 100)}
         
         ></div>
     );
