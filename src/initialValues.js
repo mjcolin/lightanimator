d4fd7f6c51c3bcee32b0bbc7159407793cadb48d
mjcolin <81725610+mjcolin@users.noreply.github.com>
2021-05-25 20:37:21 -0400

    added initial values as seperate file

---
+++ src/initialValues.js
@@ -0,0 +1,14 @@
+const initValues = 
+    [
+        {
+          pixels: [ {hue: 'red', opacity: 50},
+                    {hue: 'blue', opacity: 90},
+                    {hue: 'red', opacity: 100},
+                    {hue: 'cyan', opacity: 10},
+                    {hue: 'red', opacity: 100},
+                    {hue: 'red', opacity: 100}  ]
+        }
+    ]
+  
+
+  export default initValues;
