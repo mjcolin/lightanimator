6f3f80e9afdcb5a7d22fc0b89fff01ec2593ddab
mjcolin <81725610+mjcolin@users.noreply.github.com>
2021-05-30 22:19:34 -0400

    frame dynamically resizes

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,30 +1,42 @@
 import classes from './Frame.module.css'
 
 function Frame(props){
+    const pixelWidth = 20;
+    const numberOfPixes = props.pixels.length;
+    const pixelGap = 4;
+    const buttonWidth = 30;
+    const frameNumberWidth = 20;
+
+    const calculatedFramwWidth = frameNumberWidth+(pixelWidth+pixelGap)*numberOfPixes+buttonWidth;
+
     let thePixels = null;
+    let deleteButton = null;
 
     if(props.onClick != null){
          thePixels = props.pixels.map((pixel, index)=>
         <div className={classes.pixel} 
-        style={{backgroundColor: pixel.hue, opacity: pixel.opacity+'%'}} 
+        style={{backgroundColor: pixel.hue, opacity: pixel.opacity+'%', width: pixelWidth, height: pixelWidth, gridTemplateColumns: 'repeat(auto-fit, pixelWidth)'}} 
         key={pixel.hue+index}
         onClick={()=>props.onClick(props.frame,index, (pixel.hue === 'yellow') ? 'black' : 'yellow', 100)}
         ></div>
          );
+
+          deleteButton = <button onClick={()=>props.deleteFrame(props.frame)}>-</button>
     }
     else{
         //no click handler
         thePixels = props.pixels.map((pixel, index)=>
         <div className={classes.pixel} 
-        style={{backgroundColor: pixel.hue, opacity: pixel.opacity+'%'}} 
+        style={{backgroundColor: pixel.hue, opacity: pixel.opacity+'%',width: pixelWidth, height: pixelWidth}} 
         key={pixel.hue+index}></div>
          );
     }
 
     return(
-            <div className={classes.frame}>
+            <div className={classes.frame} style={{width: calculatedFramwWidth, columnGap:pixelGap}}>
+                {props.frame}
                 {thePixels}
-                <button onClick={()=>props.deleteFrame(props.frame)}>-</button>
+                {deleteButton}
             </div>
     )
 }
