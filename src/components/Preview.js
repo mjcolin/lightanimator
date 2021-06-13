5719be54c0b058cbadb22e93dca159b0a35caa11
mjcolin <81725610+mjcolin@users.noreply.github.com>
2021-06-13 13:51:46 -0400

    preview now updates dynamically while editing

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,51 +3,52 @@ import Container from './UI/Container.js'
 import {useState} from 'react'
 import classes from './Preview.module.css';
 import Button from './UI/Button.js'
+import useInterval from './CustomHooks/useInterval.js'
 
 function Preview(props){
     const [currentFrame, setCurrentFrame] =useState(props.frames[0].pixels);
-    const [previewID, setPreviewID] =useState(0);
+    const [playing, setPlaying] = useState(false);
+    const [currentFrameNumber, setCurrentFrameNumber] = useState(0);
     
     const frameRate = props.frameRate;
+
+    useInterval(tick, frameRate);//creates a tick for the frame rate
 
     function setFrameRate(aValue){
         props.updateFrameRate(aValue);
     }
     
     function startPreviewAnimation(){
-        
-        var frameNumber = 0;
+        setPlaying(true);
+        setCurrentFrameNumber(0);
+    }
 
-        clearInterval(previewID)
-        let aPreviewID = setInterval(tick, frameRate);
-        setPreviewID(aPreviewID);
-
-        function tick(){
-            
-            if (frameNumber >= props.frames.length){
-                frameNumber = 0;
+    function tick(){
+        if(playing){
+            const frames = props.frames;
+            const frameNumber = currentFrameNumber;
+            if (frameNumber >= frames.length){
+                setCurrentFrameNumber(0);
             }
             else{
-                if(props.frames[frameNumber] != null){
-                    setCurrentFrame(props.frames[frameNumber].pixels);
-                    frameNumber++;
+                if(frames[frameNumber] != null){
+                    setCurrentFrame(frames[frameNumber].pixels);
+                    setCurrentFrameNumber(frameNumber+1);
                 }
             }
         }
-
     }
 
+
     function stopPreviewAnimation(){
-        console.log('animation should stop of id: '+previewID)
-        clearInterval(previewID);
+        setPlaying(false);
     }
 
     function updateFrameRate(e){
         const framesPerSecond = e.target.value;
-
         //convert to milliseconds delay
         setFrameRate(1000/framesPerSecond);
-        stopPreviewAnimation();
+        //stopPreviewAnimation();
     }
 
 
@@ -66,7 +67,8 @@ function Preview(props){
          <Frame pixels={currentFrame} />
          <Frame pixels={currentFrame} />
          <div className={classes.controls}>
-            <Button onClick={startPreviewAnimation}>Play</Button>
+            <Button onClick={startPreviewAnimation} 
+                backColor={playing === true ? 'grey':''}>Play</Button>
             <Button onClick={stopPreviewAnimation} backColor="red">Stop</Button>
             <div className={classes.controlItem}>
                 <label>Frames Per a Second</label>
